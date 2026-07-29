ac963e404c77d0b2f81c2f079b6208b633612990
Neykoor <cafecafirexos@gmail.com>
2026-07-28 21:39:57 -0700

    Add files via upload

---
+++ ts/install.ts
@@ -0,0 +1,11 @@
+import { execFileSync } from "child_process";
+import { getPrebuildPath } from "../binding";
+
+function main(): void {
+  if (getPrebuildPath()) {
+    process.exit(0);
+  }
+  execFileSync("node-gyp", ["rebuild"], { stdio: "inherit" });
+}
+
+main();
