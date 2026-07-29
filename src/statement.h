1e18f8a1a34bcfdd3f6cd14b9092ea4507b55ac9
Neykoor <cafecafirexos@gmail.com>
2026-07-28 21:06:40 -0700

    Add files via upload

---
+++ src/statement.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <napi.h>
+#include "deps/sqlite3/sqlite3.h"
+
+class InfinityDatabase;
+
+class InfinityStatement : public Napi::ObjectWrap<InfinityStatement> {
+public:
+  static Napi::Function GetClass(Napi::Env env);
+  InfinityStatement(const Napi::CallbackInfo &info);
+  ~InfinityStatement();
+
+  static Napi::Object Create(Napi::Env env, InfinityDatabase *owner, sqlite3_stmt *stmt, bool pluckMode);
+  void CloseByOwner();
+
+private:
+  Napi::Value Run(const Napi::CallbackInfo &info);
+  Napi::Value Get(const Napi::CallbackInfo &info);
+  Napi::Value All(const Napi::CallbackInfo &info);
+  Napi::Value Iterate(const Napi::CallbackInfo &info);
+  Napi::Value Pluck(const Napi::CallbackInfo &info);
+  Napi::Value Finalize(const Napi::CallbackInfo &info);
+  Napi::Value ColumnNames(const Napi::CallbackInfo &info);
+
+  bool EnsureUsable(Napi::Env env);
+  void BindAll(const Napi::CallbackInfo &info, size_t startIndex);
+  void BindOne(int index, const Napi::Value &value);
+  Napi::Object RowToObject(Napi::Env env);
+  Napi::Value RowToPluck(Napi::Env env);
+
+  InfinityDatabase *owner_;
+  sqlite3 *db_;
+  sqlite3_stmt *stmt_;
+  bool pluck_;
+  bool finalized_;
+  bool iterating_;
+};
